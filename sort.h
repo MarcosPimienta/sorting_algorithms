c79061c7b2de1903f67e3e44893b53d0aeaf6229
Camilo Ramirez escobar <camilo@Camilos-MacBook-Pro.local>
2020-10-19 17:47:53 -0500

    updater

--- sort.h
+++ sort.h
@@ -1,5 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -7,6 +10,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
@@ -16,4 +20,5 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 #endif

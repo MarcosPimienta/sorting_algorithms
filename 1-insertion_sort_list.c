6113ff8c15ee8860c845ea23e0a9c004a51ecabf
MarcosPimienta <1676@holbertonschool.com>
2020-10-20 20:35:21 +0000

    fixed betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,35 @@
 #include "sort.h"
-
+/**
+ * insertion_sort_list - sorting algorithm.
+ * @list: pointer to the head of the list
+ * Return: void
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current;
-    current = *list;
-    current  = current->next;
+	listint_t *current;
 
-    while(current != NULL)
-    {
-        while(current != NULL)
-        {
-            if (current->n < current->prev->n)
-            {
-                if (current->prev->prev == NULL)
-                {
-                    current->next->prev = current->prev;
-                    current->prev->next = current->next;
-                    current->prev->prev = current;
-                    current->next = current->prev;
-                    current->prev = NULL;
-                    *list = current;
-                    print_list(*list);
-                    break;
-                }
-                if (current->next == NULL)
-                {
-                    current->prev = current->prev->prev;
-                    current->next = current->prev->next;
-                    current->next->prev = current;
-                    current->prev->next = current;
-                    current->next->next = NULL;
-                    print_list(*list);
-                }
-                current->prev = current->prev->prev;
-                current->prev->next->prev = current;
-                current->next->prev = current->prev->next;
-                current->prev->next->next = current->next;
-                current->next = current->prev->next;
-                current->prev->next = current;
-                print_list(*list);
-            }
-            else
-            {
-                break;
-            }
-        }
-        current = current->next;
-    }
+	if ((*list)->next == NULL || list == NULL)
+	{
+		return;
+	}
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->prev->next = current->next;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }

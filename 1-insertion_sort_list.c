f06c505c2729bf2f8b03dea098964210e5838a81
MarcosPimienta <1676@holbertonschool.com>
2020-10-20 01:57:54 +0000

    condition added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,37 +2,36 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current;
-    current = *list;
-    current  = current->next;
+        listint_t *current, *temp;
+        current = *list;
 
-    while(current->next != NULL)
-    {
         while(current->next != NULL)
         {
-            if (current->n < current->prev->n)
-            {
-                /**
-                if (current->prev->prev == NULL)
+                temp = current;
+                while(temp != NULL)
                 {
-                    current->prev->next = current->next;
-                    current->prev->prev = current;
-                    current->prev = NULL;
-                    break;
+                        if (current->n < current->prev->n)
+                        {
+                                /**
+                                if (current->prev->prev == NULL)
+                                {
+                                current->prev->next = current->next;
+                                current->prev->prev = current;
+                                current->prev = NULL;
+                                break;
+                                }
+                                */
+                                current->prev = current->prev->prev;
+                                current->prev->next->prev = current;
+                                current->next->prev = current->prev->next;
+                                current->prev->next->next = current->next;
+                                current->next = current->prev->next;
+                                current->prev->next = current;
+                        }
+                        else
+                                break;
+                temp = temp->prev;
                 }
-                */
-                current->prev = current->prev->prev;
-                current->prev->next->prev = current;
-                current->next->prev = current->prev->next;
-                current->prev->next->next = current->next;
-                current->next = current->prev->next;
-                current->prev->next = current;
-            }
-            else
-            {
-                break;
-            }
-        }
         current = current->next;
-    }
+        }
 }

fdfe8c307ce0306f558c13e47433c6c717224d82
Camilo Ramirez escobar <camilo@Camilos-MacBook-Pro.local>
2020-10-20 10:51:28 -0500

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,32 +1,48 @@
 #include "sort.h"
-
+/**
+ * dasdsadasd
+ * dsdadadasd
+ * sadsadasdasd
+ * asdadasd
+ */
 void insertion_sort_list(listint_t **list)
 {
     listint_t *current;
     current = *list;
     current  = current->next;
 
-    while(current->next != NULL)
+    while(current != NULL)
     {
-        while(current->next != NULL)
+        while(current != NULL)
         {
             if (current->n < current->prev->n)
             {
-                /**
+                if (current->next == NULL)
+                {
+                    current->prev = current->prev->prev;
+                    current->next = current->prev->next;
+                    current->next->prev = current;
+                    current->next->next = NULL;
+                }
                 if (current->prev->prev == NULL)
                 {
+                    current->next->prev = current->prev;
                     current->prev->next = current->next;
                     current->prev->prev = current;
+                    current->next = current->prev;
                     current->prev = NULL;
+                    *list = current;
+                    print_list(*list);
                     break;
                 }
-                */
+
                 current->prev = current->prev->prev;
                 current->prev->next->prev = current;
                 current->next->prev = current->prev->next;
                 current->prev->next->next = current->next;
                 current->next = current->prev->next;
                 current->prev->next = current;
+                print_list(*list);
             }
             else
             {

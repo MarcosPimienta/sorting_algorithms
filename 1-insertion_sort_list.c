2fd576317cf6719fc58ca197b15ac9217df92fa6
MarcosPimienta <1676@holbertonschool.com>
2020-10-20 21:06:19 +0000

    fixed betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,16 +8,12 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
 
-	if ((*list)->next == NULL || list == NULL)
-	{
+	if (list == NULL || (*list)->next == NULL)
 		return;
-	}
-
-	current = (*list)->next;
-
+	current =  (*list)->next;
 	while (current != NULL)
 	{
-		while (current->prev != NULL && current->n < current->prev->n)
+		while (current->prev && current->n < current->prev->n)
 		{
 			if (current->next != NULL)
 				current->next->prev = current->prev;
@@ -26,15 +22,13 @@ void insertion_sort_list(listint_t **list)
 			current->prev = current->prev->prev;
 			current->next->prev = current;
 			if (current->prev == NULL)
-			{
 				*list = current;
-			}
 			else
 			{
 				current->prev->next = current;
 			}
 			print_list(*list);
 		}
-		current = current->next;
+	current = current->next;
 	}
 }
